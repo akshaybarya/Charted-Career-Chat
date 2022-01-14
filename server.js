71b212c57c57aff92ad498989ff638ac389364d6
Akshay Kumar Barya <76679278+akshaybarya@users.noreply.github.com>
2022-01-14 20:00:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 db();
 
-/* NODEMAILER */
+/*  NODEMAILER */
 
 const sendMail = async ({ name, email, phone }) => {
   const transporter = nodemailer.createTransport({
